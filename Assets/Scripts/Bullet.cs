09d0bb6b20d61e07582db46b570b3a9aa7f36494
mate444 <mateoeo23@gmail.com>
2022-02-14 14:23:35 -0300

    Solucionado problema con el metodo FireBarrage

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,15 +1,13 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour {
-    public Vector3 direction;
     public int speed = 20;
   void Start() {
         
   }
 
   void Update() {
-    transform.Translate(direction * Time.deltaTime * speed);
+    transform.position += transform.forward * speed * Time.deltaTime;
   }
 }
--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -1,18 +1,32 @@
+using System;
 using UnityEngine;
 
 public class Cannon : MonoBehaviour {
   public GameObject prefab;
-  public Vector3 pos;
+  public GameObject spawnPoint;
 
   void Start() {
       Fire();
-      Debug.Log(pos);
   }
 
   void Update() {
-    
+    if (Input.GetKeyDown("j")) {
+        FireBarrage(2);
+      }
+      if (Input.GetKeyDown("k")) {
+        FireBarrage(3);
+      }
+      if (Input.GetKeyDown("l")) {
+        FireBarrage(4);
+      }
   }
   void Fire() {
-    Instantiate(prefab, transform.position, transform.rotation);
+    Instantiate(prefab, spawnPoint.transform.forward, spawnPoint.transform.rotation);
+  }
+  void FireBarrage(int times) {
+    for (int i = 0; i < times; i++)
+    {
+      Instantiate(prefab, spawnPoint.transform.position, spawnPoint.transform.rotation);
+    }
   }
 }
