ef2ded3a9e5ef25c5a4a5e4c8fff45fb563dc156
mate444 <mateoeo23@gmail.com>
2022-02-26 11:41:59 -0300

    Added camera and moving mechanic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
   public Vector3 scale;
-  public float speed;
+  public float speed = 4f;
+  public GameObject thirdPersonCamera;
+  public GameObject skyCamera;
+  public GameObject outfitCamera;
   public Vector3 position;
 
   void Start() {
@@ -19,6 +22,41 @@ public class Player : MonoBehaviour {
   }
 
   void Update() {
-      transform.position += position * speed * Time.deltaTime;
+      Move();
+      ToggleCamera();
+  }
+  void Move() {
+    if (Input.GetKey(KeyCode.W)) {
+      transform.Translate(new Vector3(-0.1f, 0, 0));
+    }
+    if (Input.GetKey(KeyCode.S)) {
+      transform.Translate(new Vector3(0.1f, 0, 0));
+    }
+    if (Input.GetKey(KeyCode.A)) {
+      transform.Translate(new Vector3(0, 0, -0.1f));
+    }
+    if (Input.GetKey(KeyCode.D)) {
+      transform.Translate(new Vector3(0, 0, 0.1f));
+    }
+    /*float horizontal = Input.GetAxis("Horizontal");
+    float vertical = Input.GetAxis("Vertical");
+    transform.Translate(new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime);*/
+  }
+  void ToggleCamera() {
+    if (Input.GetKey(KeyCode.V)) {
+      thirdPersonCamera.SetActive(true);
+      skyCamera.SetActive(false);
+      outfitCamera.SetActive(false);
+    }
+    if (Input.GetKey(KeyCode.B)) {
+      thirdPersonCamera.SetActive(false);
+      skyCamera.SetActive(true);
+      outfitCamera.SetActive(false);
+    }
+    if (Input.GetKey(KeyCode.N)) {
+      thirdPersonCamera.SetActive(false);
+      skyCamera.SetActive(false);
+      outfitCamera.SetActive(true);
+    }
   }
 }
